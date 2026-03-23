63d54f68459108ce0d56a5c95bd0c52c53f275a2
Carlos Neto <106997150+carlosbne@users.noreply.github.com>
2026-03-23 15:22:35 +0000

    adicionando teste de metodos Pilhaarray

--- testes-algoritmo/testePilha.java
+++ testes-algoritmo/testePilha.java
@@ -1,10 +1,10 @@
 public class testePilha {
     
     public static void main(String[] args){
-        Integer[] b = new Integer[1];
-        PilhaArray pp = new PilhaArray(1, 0);
+        /* Integer[] b = new Integer[1]; */
+        PilhaArray pp = new PilhaArray(1, 5);
         System.out.println("inserindo");
-        for(int f=0; f<10; f++){
+        for(int f=0; f<15; f++){
             System.out.println(f);
             pp.push(f);
         }
@@ -13,5 +13,7 @@ public class testePilha {
             System.out.print(f);
             System.out.println(" - " + pp.pop());
         }
+        System.out.println("tamanho: " + pp.size());
+        System.out.println("topo: " + pp.top());
     }
 }
